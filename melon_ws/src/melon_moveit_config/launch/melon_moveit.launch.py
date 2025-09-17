2cf770427e9171421671185e956626f4b1257e83
SSatoya <c0b230860d@edu.teu.ac.jp>
2025-09-17 03:10:07 +0000

    commit

--- melon_ws/src/melon_moveit_config/launch/melon_moveit.launch.py
+++ melon_ws/src/melon_moveit_config/launch/melon_moveit.launch.py
@@ -1,5 +1,21 @@
 # This code was taken from the following URL:
 # https://github.com/isaac-sim/IsaacSim-ros_workspaces/blob/main/humble_ws/src/moveit/isaac_moveit/launch/isaac_moveit.launch.py
+
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
